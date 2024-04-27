fac156bf0f860044fd2cf9c6b48df8f972bdc2d1
brettc02 <145397897+brettc02@users.noreply.github.com>
2024-04-26 20:42:58 -0400

    Added H1 Heading, Added filtered data logic, set up onleave logic, added toggle button with label

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,34 @@ import MediaCard from './components/employee-card.jsx';
 import Container from '@mui/material/Container';
 import Data from './data/data.js';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 function App() {
-  const [onLeave, setLeave] = useState(true)
-console.log(onLeave);
+  const [showOnLeave, setShowOnLeave] = useState(false)
+console.log(showOnLeave);
+
+const filteredData = showOnLeave ? Data.filter((employee) => employee.onLeave) : Data;
+
+const toggleOnLeave = (event) => { if(showOnLeave) {setShowOnLeave(false)} else setShowOnLeave(true)};
+
   return (
     <Container fixed>
-    <Button variant="contained" sx={{ marginRight: 2 }}>Active</Button>
-    <Button variant="outlined">On Leave</Button>
-      <MediaCard data={Data} />
+      <Box>
+       <Typography variant="h4" component="h1"gutterBottom>
+        Employee Dashboard
+      </Typography>
+      <Stack spacing={2} direction="row" justifyContent="center">
+      <FormGroup>
+      <FormControlLabel control={<Switch default onChange={toggleOnLeave}/>} label="On Leave" />
+    </FormGroup>
+    </Stack>
+      <MediaCard data={filteredData} />
+      </Box>
     </Container>
   )
 }
